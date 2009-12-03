ce6fc7afe15f85c056157ca2564740a5442e7999
Rusty Burchfield <rusty@heyzap.com>
2009-12-03 08:57:27 -0800

    Fix a typo and make keys public

--- lib/authlogic_facebook.rb
+++ lib/authlogic_facebook.rb
@@ -5,5 +5,5 @@ require 'authlogic_facebook/helper'
 if ActiveRecord::Base.respond_to?(:add_acts_as_authentic_module)
   ActiveRecord::Base.send(:include, AuthlogicFacebook::ActsAsAuthentic)
   Authlogic::Session::Base.send(:include, AuthlogicFacebook::Session)
-  ActionController::Base.helper AuthlogicFacebookConnect::Helper
+  ActionController::Base.helper AuthlogicFacebook::Helper
 end
